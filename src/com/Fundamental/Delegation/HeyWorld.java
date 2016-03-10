663c8af2bcf258a69d72515e536500db6ae09df5
kohmar <kohmarkus@gmail.com>
2016-03-10 21:30:45 +0200

    my second commite

---
+++ src/com/Fundamental/Delegation/HeyWorld.java
@@ -0,0 +1,10 @@
+package com.Fundamental.Delegation;
+
+/**
+ * @author  Plavun on 10.03.2016.
+ */
+public class HeyWorld {
+    public static void main(String[] args) {
+        System.out.println("hey");
+    }
+}
